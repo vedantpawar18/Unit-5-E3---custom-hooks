7ced6d7ea60d9b90391112c4be712596e27301ff
vedantpawar18 <vedantpawar18@gmail.com>
2022-08-16 14:05:04 +0530

    E3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Image } from "./Components/Image";
+import { Input } from "./Components/Input";
+import { useCounter } from "./hooks/useCounter";
 
 function App() {
+  /* 
+  1. Implement only the hook functionality inside hooks/useCounter.js
+  2. Create the custom components, inside the respective files of Image.jsx and Image.jsx
+  3. The application should function properly after the implimentation of hooks and Custom components
+  */
+  const { countValue, incCount, decCount } = useCounter({
+    initialValue: 10,
+    minValue: 5,
+    maxValue: 15,
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* DO NOT CHANGE the 5 lines below   */}
+      <h3>Value: {countValue}</h3>
+      <button onClick={() => incCount()}>Increment</button>
+      <button onClick={() => incCount(3)}>Increment 3</button>
+      <button onClick={() => decCount(2)}>Decrement 2</button>
+      <button onClick={() => decCount(4)}>Decrement 4</button>
+
+      <br />
+      {/* You can pass the required props as mentioned in the questions to
+      check if the components are working properly */}
+      <Image />
+      <br />
+      <Input />
     </div>
   );
 }
